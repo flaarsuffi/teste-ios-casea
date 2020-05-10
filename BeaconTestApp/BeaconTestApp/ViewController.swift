2cb9782569a6db67669b31bf76ded88882d997a5
Flavia Arsuffi <flavia.arsuffi@gmail.com>
2020-05-10 11:36:56 -0300

    first commit

---
+++ BeaconTestApp/BeaconTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeaconTestApp
+//
+//  Created by Flavia Arsuffi on 10/05/20.
+//  Copyright © 2020 Flavia Arsuffi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
